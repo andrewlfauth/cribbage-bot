5e7be5bb857c7b9f986259d18dbe0f099e0cc0b5
andrewlfauth <andrewlfauth@gmail.com>
2023-01-07 19:41:33 -0800

    simplify objectEquals function
    checking Object.keys was returning Type Errors sometimes. To avoid this I went with the less optmiized JSON stringify method of comparing two objects. I will attempt to debug the other solution in the future.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,2 @@
 export const objectsEqual = (o1, o2) =>
-  Object.keys(o1).length === Object.keys(o2).length &&
-  Object.keys(o1).every((p) => o1[p] === o2[p])
+  JSON.stringify(o1) === JSON.stringify(o2)
