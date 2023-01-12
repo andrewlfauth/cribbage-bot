5af82a85cc5b4f1d96f5622b9dac002f9b968f13
andrewlfauth <andrewlfauth@gmail.com>
2023-01-11 21:02:41 -0800

    add indicator when turn is a 'Go'

---
+++ src/components/GameView/GoIndicator.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  player: String,
+  show: Boolean,
+})
+</script>
+
+<template>
+  <div
+    :class="{
+      '-top-16': player == 'bot',
+      '-bottom-16': player == 'user',
+      'opacity-100': props.show,
+      'opacity-0': !props.show,
+    }"
+    class="absolute left-0 right-0 mx-auto w-20 bg-black border-2 border-white rounded-md text-white text-center text-2xl py-1 duration-300"
+  >
+    Go
+  </div>
+</template>
--- src/components/GameView/Pegging.vue
+++ src/components/GameView/Pegging.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { watchEffect, watch, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { game, startPeggingStage } from '../../stores/game'
 import Card from '../Card.vue'
+import GoIndicator from './GoIndicator.vue'
 
 onMounted(() => {
   startPeggingStage()
@@ -9,13 +10,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <p class="absolute top-12 left-0 font-semibold pl-2">
-    {{ game.pegging.turn }}'s turn
-  </p>
-  <p class="absolute top-0 left-0 font-semibold pl-2">
-    {{ game.pegging.opponent }}
-  </p>
   <div class="relative w-[700px] h-[180px] border border-white rounded-md">
+    <GoIndicator
+      :show="game.pegging.turn === 'user' && game.pegging.opponent === 'go'"
+      :player="'bot'"
+    />
     <div class="flex -space-x-8">
       <Card v-for="card in game.pegging.cards" :card="card" />
     </div>
@@ -25,5 +24,9 @@ onMounted(() => {
       <span>Pegging count:</span>
       <span class="text-4xl font-semibold">{{ game.pegging.count }}</span>
     </div>
+    <GoIndicator
+      :show="game.pegging.turn === 'bot' && game.pegging.opponent === 'go'"
+      :player="'user'"
+    />
   </div>
 </template>
